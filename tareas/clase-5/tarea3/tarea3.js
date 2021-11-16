f6c758c530606e03268946543ec0506b2e573f6b
mefGauto <gautomatiaz@gmail.com>
2021-11-16 13:38:35 -0300

    Escribe la duracion del video desde js

--- tareas/clase-5/tarea3/tarea3.js
+++ tareas/clase-5/tarea3/tarea3.js
@@ -1,16 +1,13 @@
 document.querySelector('#botonSumar').onclick = function(){
-  const horas = document.createTextNode(document.querySelector('#horas').value);
-  const minutos = document.createTextNode(document.querySelector('#minutos').value);
-  const segundos = document.createTextNode(document.querySelector('#segundos').value);
+  const horas = document.querySelector('#horas').value;
+  const minutos = document.querySelector('#minutos').value;
+  const segundos = document.querySelector('#segundos').value;
 
-  const nodoP = document.querySelector('#duracionTotal');
-  const strongHoras = document.querySelector('#strong-horas');
-  const strongMinutos = document.querySelector('#strong-minutos');
-  const strongSegundos = document.querySelector('#strong-segundos');
-  strongHoras.appendChild(horas);
-  strongMinutos.appendChild(minutos);
-  strongSegundos.appendChild(segundos);
-  nodoP.appendChild(strongHoras);
-  nodoP.appendChild(strongMinutos);
-  nodoP.appendChild(strongSegundos);
+  let strongHoras = document.querySelector('#strong-horas');
+  let strongMinutos = document.querySelector('#strong-minutos');
+  let strongSegundos = document.querySelector('#strong-segundos');
+  let nodoP = document.querySelector('#parrafo');
+
+  nodoP.textContent = (`Tu video dura: ${horas} horas, ${minutos} minutos y ${segundos} segundos.`)
+  
 }
