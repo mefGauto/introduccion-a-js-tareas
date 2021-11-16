730b21617f003dceccdbfe0da5267d83caad1dbc
mefGauto <gautomatiaz@gmail.com>
2021-11-16 14:40:26 -0300

    Añade la duracion de los videos a una lista pre-hecha

--- tareas/clase-5/tarea3/tarea3.js
+++ tareas/clase-5/tarea3/tarea3.js
@@ -1,10 +1,14 @@
+let nodoP = document.querySelector('#parrafo');
+let lista = document.querySelector('#listaVideos')
+let video;
+
 document.querySelector('#botonSumar').onclick = function(){
   const horas = document.querySelector('#horas').value;
   const minutos = document.querySelector('#minutos').value;
   const segundos = document.querySelector('#segundos').value;
 
-  let nodoP = document.querySelector('#parrafo');
-
-  nodoP.textContent = (`Tu video dura: ${horas} horas, ${minutos} minutos y ${segundos} segundos.`)
-  
+  let nodoLi = document.createElement('li');
+  video = document.createTextNode( `Tu video dura: ${horas} horas, ${minutos} minutos y ${segundos} segundos.`);
+  nodoLi.appendChild(video);
+  lista.appendChild(nodoLi);
 }
