e44eca5cfde1ee49d9a2b2e12796ebcf91cf7b6b
mefGauto <gautomatiaz@gmail.com>
2021-11-16 13:45:32 -0300

    Actualiza el .html y .js

--- tareas/clase-5/tarea3/tarea3.js
+++ tareas/clase-5/tarea3/tarea3.js
@@ -3,9 +3,6 @@ document.querySelector('#botonSumar').onclick = function(){
   const minutos = document.querySelector('#minutos').value;
   const segundos = document.querySelector('#segundos').value;
 
-  let strongHoras = document.querySelector('#strong-horas');
-  let strongMinutos = document.querySelector('#strong-minutos');
-  let strongSegundos = document.querySelector('#strong-segundos');
   let nodoP = document.querySelector('#parrafo');
 
   nodoP.textContent = (`Tu video dura: ${horas} horas, ${minutos} minutos y ${segundos} segundos.`)
