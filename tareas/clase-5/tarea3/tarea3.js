d911e2c21356a416fb63320ecf8deec6fbefe6df
mefGauto <gautomatiaz@gmail.com>
2022-04-25 12:19:12 -0300

    Intento fallido de terminar esta tarea

--- tareas/clase-5/tarea3/tarea3.js
+++ tareas/clase-5/tarea3/tarea3.js
@@ -1,14 +1,32 @@
-let nodoP = document.querySelector('#parrafo');
-let lista = document.querySelector('#listaVideos')
+let strong = document.querySelector('#duracionTotal');
+let lista = document.querySelector('#listaVideos');
 let video;
+let tiempoSumado;
 
 document.querySelector('#botonSumar').onclick = function(){
-  const horas = document.querySelector('#horas').value;
-  const minutos = document.querySelector('#minutos').value;
-  const segundos = document.querySelector('#segundos').value;
+  let horas = document.querySelector('#horas').value;
+  let minutos = document.querySelector('#minutos').value;
+  let segundos = document.querySelector('#segundos').value;
+  
+  let horasGuardadas = 0;
+  let minutosGuardados = 0;
+  let segundosGuardados = 0;
 
   let nodoLi = document.createElement('li');
   video = document.createTextNode( `Tu video dura: ${horas} horas, ${minutos} minutos y ${segundos} segundos.`);
   nodoLi.appendChild(video);
   lista.appendChild(nodoLi);
+
+  function sumarTiempo(){
+    let horasSumadas = horasGuardadas +++ horas;
+    let minutosSumados = minutosGuardados +++ minutos;
+    let segundosSumados = segundosGuardados +++ segundos;
+
+    let parrafo = document.createElement('p');
+    tiempoSumado = document.createTextNode(`El tiempo total es: ${horasSumadas} horas, ${minutosSumados} minutos, ${segundosSumados} segundos.`);
+    parrafo.appendChild(tiempoSumado);
+    strong.appendChild(parrafo);
+  }
+
+  sumarTiempo();
 }
