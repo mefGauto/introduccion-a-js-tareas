f565a4baadb386097e7837820961c504195e2965
mefGauto <gautomatiaz@gmail.com>
2022-06-17 17:41:26 -0300

    FizzBuzz usando elementos de HTML y el DOM Este codigo imprime en un parrafo guardado en un div un numero o "FizzBuzz" si ese numero es divisible por 3 y 5, "Fizz" si es divisible solo por 3 y "Buzz" si solo es divisible por 5.

--- tareas/clase-4/tarea-clase-4.js
+++ tareas/clase-4/tarea-clase-4.js
@@ -20,16 +20,20 @@ function tarea2 () {
 
 }
 tarea2();
-/*
-function tarea2 (){
-    let num = 11;
-    while (num > 1){
-        num--;
-        console.log(num);
-    }
-}
-tarea2();
-/*
+                            /*
+                            function tarea2 (){
+                                let num = 11;
+                                while (num > 1){
+                                    num--;
+                                    console.log(num);
+                                }
+                            }
+                            tarea2();
+                            /*
+
+
+
+
 // TAREA: Ha llegado el momento de un ejercicio clásico: 'FizzBuzz'.
 // Cuenta del 1 al 50 e imprime los números:
 // * Si un número es múltiplo de tres, imprime 'Fizz'.
@@ -41,30 +45,56 @@ tarea2();
 // 10% 3 = 1 - en 10 tenemos 3 * 3 + 1
 // 16% 4 = 0 - en 16 tenemos 4 * 4
 // 19% 4 = 3 - en 19 tenemos 4 * 4 + 3, etc.
- /*
-function fizzBuzz(){
-    for(let i = 1; i <= 50; i++){
-        if (i % 3 === 0 && i % 5 === 0){
-            console.log('FizzBuzz');
-        }else if(i % 3 === 0){
-            console.log('Fizz');
-        }else if(i % 5 === 0){
-            console.log('Buzz');
-        }else{
-            console.log(i);
+                        
+                        /*
+                        function fizzBuzz(){
+                            for(let i = 1; i <= 50; i++){
+                                if (i % 3 === 0 && i % 5 === 0){
+                                    console.log('FizzBuzz');
+                                }else if(i % 3 === 0){
+                                    console.log('Fizz');
+                                }else if(i % 5 === 0){
+                                    console.log('Buzz');
+                                }else{
+                                    console.log(i);
+                                }
+                            }
+                        }
+                        fizzBuzz();
+                        */
+let div = document.querySelector('#FizzBuzz');
+let parrafo = document.createElement('p');
+div.appendChild(parrafo);
+function fizzBuzz () {
+    for (let i = 0; i <= 50; i++){
+        if (i % 5 === 0 && i % 3 === 0) {
+            let fBuzz = document.createTextNode('FizzBuzz \n');
+            parrafo.appendChild(fBuzz);
+        }else if (i % 5 === 0) {
+            let buzz = document.createTextNode('buzz \n');
+            parrafo.appendChild(buzz);
+        }else if (i % 3 === 0) {
+            let fizz = document.createTextNode('fizz \n');
+            parrafo.appendChild(fizz);
+        }else {
+            let num = document.createTextNode(`${i} \n`);
+            parrafo.appendChild(num);
         }
+
     }
 }
 fizzBuzz();
- */
 // Calcular el promedio de todos los números en un array de números. (y ponerlo en una función)
 // ej.: calcularPromedio([10,5,4,2,8])
-function calcularPromedio(){
-    const numeros = [10,5,4,2,8];
-    let totalNumeros = 0;
-    for(let i = 0; i < numeros.length; i++){
-        console.log(totalNumeros = totalNumeros + numeros[i]);
-    }
-    console.log(totalNumeros / numeros.length)
-}
-calcularPromedio();
+
+
+                /*function calcularPromedio(){
+                    const numeros = [10,5,4,2,8];
+                    let totalNumeros = 0;
+                    for(let i = 0; i < numeros.length; i++){
+                        console.log(totalNumeros = totalNumeros + numeros[i]);
+                    }
+                    console.log(totalNumeros / numeros.length)
+                }
+                calcularPromedio();
+                */
