3eacd944e2f3c27660bd3bb26d82d8d575989da8
mefGauto <gautomatiaz@gmail.com>
2022-06-18 15:57:29 -0300

    Tarea 4, Clase 5: Añade mas funciones Ahora cuando el usuario hace click en "Subir" para añadir un nuevo numero a la lista en lugar de ejecutar el codigo directamente en el click, se llama a otras funciones que ejecuten lo que pide la tarea

--- tareas/clase-5/tarea4/tarea4.js
+++ tareas/clase-5/tarea4/tarea4.js
@@ -5,8 +5,11 @@
 // 2. obtener el número más pequeño y mostrarlo en un <em> pre-creado con el texto "El número más pequeño es..."
 // 3. obtener el número más grande y mostrarlo en un <em> pre-creado con el texto "El número más grande es..."
 // 4. obtener el número que más se repite y mostrarlo en un <em> pre-creado con el texto "El número más frecuente es..."
+
+
 let listaNumeros = document.querySelector('#lista-ordenada');
 let cantidadNumeros = [];
+
 document.querySelector('#btn-subir').onclick = function() {
     let nodoLi = document.createElement('li');
     let inputNumero = Number(document.querySelector('#input-numero').value);
@@ -17,6 +20,20 @@ document.querySelector('#btn-subir').onclick = function() {
 
     cantidadNumeros.push(inputNumero);
 
+    obtenerPromedio();
+
+    obtenerNumeroMinimo();
+
+    obtenerNumeroMaximo();
+
+    obtenerNumeroMasFrecuente();
+
+    return false;
+}
+
+
+function obtenerPromedio(){
+
     let mensajeDelPromedio = document.querySelector('#promedio');
     let totalNumeros = 0;
     for (let i = 0; i < cantidadNumeros.length; i++){
@@ -25,13 +42,25 @@ document.querySelector('#btn-subir').onclick = function() {
     let promedioTotal = totalNumeros / cantidadNumeros.length;
     mensajeDelPromedio.textContent = `El promedio del array es: ${promedioTotal}`;
 
+}
+
+function obtenerNumeroMinimo() {
+    
     let mensajeNumeroMinimo = document.querySelector('#numero-minimo');
     let cantidadMinima = Math.min(...cantidadNumeros);
     mensajeNumeroMinimo.textContent = `El numero minimo es: ${cantidadMinima}`;
 
+}
+
+function obtenerNumeroMaximo() {
+    
     let mensajeNumeroMaximo = document.querySelector('#numero-maximo');
     let cantidadMaxima = Math.max(...cantidadNumeros);
-    mensajeNumeroMaximo.textContent = `El numero maximo es: ${numMax}`;
+    mensajeNumeroMaximo.textContent = `El numero maximo es: ${cantidadMaxima}`;
+
+}
+
+function obtenerNumeroMasFrecuente() {
 
     let mensajeNumeroFrecuente = document.querySelector('#frecuente');
     let cantidadOrdenada = [...cantidadNumeros].sort();
@@ -43,6 +72,4 @@ document.querySelector('#btn-subir').onclick = function() {
         }
     }
 
-    return false;
 }
-
